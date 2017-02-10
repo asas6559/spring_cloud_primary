53e766ce2b19be25a2e1876ceddd777b642c878c
asas6559 <302811456@qq.com>
2017-02-10 14:56:35 +0800

    add module "scpmonitor"

---
+++ spring_cloud_primary/scpmonitor/src/main/java/org/scpmonitor/App.java
@@ -0,0 +1,13 @@
+package org.scpmonitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
